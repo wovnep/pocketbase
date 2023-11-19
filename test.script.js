811b3ad48b2be012cc5b1dda051c62412d9543e6
wovnep <thinuravck@gmail.com>
2023-11-19 08:09:00 +0000

    fix: workflow version check

--- test.script.js
+++
@@ -1,7 +0,0 @@
-module.exports = async ({github, context, core}) => {
-    const commit = await github.rest.repos.getLatestRelease({
-        owner: "pocketbase",
-        repo: "pocketbase",
-    })
-    core.exportVariable('author', commit.data.commit.author.email)
-}
